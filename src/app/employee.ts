da34bb6e51fc69ec8cd75d6d7fe3a348cba03d4c
Janith Harinda <jharinda@gmail.com>
2021-06-09 18:17:48 +0530

    Created Employee Mock Data

---
+++ src/app/employee.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+    id: number,
+    firstName: string,
+    lastName: string,
+    dob: string,
+    telephone: number,
+    email: string,
+    maritalStatus: boolean,
+    city: number,
+    remark: string
+}
